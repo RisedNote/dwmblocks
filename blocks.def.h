da2a6b360b0e1038df9419bc04ef15a289737d03
RisedNote <risednote@rock.com>
2022-09-07 17:45:55 -0700

    fix clock and added seperator

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/*Icon*/	/*Command*/	        	/*Update Interval*/	        /*Update Signal*/
 	/*{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},*/
 
-	/*{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},*/
+  // clicks dont work unless ther is a update signal
+	{"", "~/.config/dwmblocks/scripts/volume",				0,		1}, 
 
-	{"", "~/.config/dwmblocks/scripts/volume",				5,		0},
-
-	{"", "~/.config/dwmblocks/scripts/time-date",     5,		0},
+	{"", "~/.config/dwmblocks/scripts/time-date",     5,		2},
 
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = ""; //i used midi left in nerd fonts
+static char delim[] = "|"; //i used midi left in nerd fonts
 static unsigned int delimLen = 5;
